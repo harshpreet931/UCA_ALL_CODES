a11ba289d181c6194b9a86e2062dbc463bcb369e
harshpreet931 <harshpreet0402@gmail.com>
2024-08-07 16:07:41 +0530

    chore: Moved some Programs to Eddential Programs, Added LinkedList Implementation

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,22 +1,87 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <limits.h>
 
-typedef struct LinkedListNode {
-    Node* node;
-    int data;
-} Node;
+struct Node {
+    int item;
+    struct Node* next;
+};
 
-Node* createNode(int data) {
-    Node* newNode = (Node *) malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
+typedef struct Node Node;
+Node* head = NULL;
+int N = 0;
+
+bool isEmpty() {
+    return N == 0;
 }
 
-void insertAtStart(
+int size() {
+    return N;
+}
+
+void push(int item) {
+    Node* newNode = (Node *) malloc(sizeof(Node));
+    newNode->item = item;
+    newNode->next = head;
+    head = newNode;
+    N++;
+}
+
+int get(int idx) {
+    if(idx < 0 || idx >= N) return INT_MIN;
+
+    Node* curr = head;
+    for(int i = 0; i < idx; i++) curr = curr->next;
+
+    return curr->item;
+}
+
+int removeAt(int idx) {
+    if(idx < 0 || idx >= N) return INT_MIN;
+
+    Node* curr = head;
+    Node* prev = NULL;
+    for(int i = 0; i < idx; i++) {
+        prev = curr; 
+        curr = curr->next;
+    }
+    
+    int item = curr->item;
+   
+    if(prev == NULL) head = curr->next;
+    else prev->next = curr->next;
+   
+    free(curr);
+    N--;
+    return item; 
+}
+    
+void testLinkedList() {
+    push(3);
+    push(10);
+    push(20);
+
+    assert(isEmpty() == false);
+    assert(size() == 3);
+    assert(get(0) == 20);
+    assert(get(1) == 10);
+    assert(get(2) == 3);
+    assert(removeAt(1) == 10);
+    assert(size() == 2);
+    assert(get(0) == 20);
+    assert(get(1) == 3);
+    assert(removeAt(0) == 20);
+    assert(size() == 1);
+    assert(get(0) == 3);
+    assert(removeAt(0) == 3);
+    assert(size() == 0);
+    assert(isEmpty());
+}
 
 int main() {
-    
-
-
+    testLinkedList();
+    printf("All tests passed!!\n");
     return 0;
 }
--- test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-	printf("Hello World! \n");
-	printf("This is from windows! \n");
-	return 0;
-}
