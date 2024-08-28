a3a5db5334bb41a59d212a3791876526d80a4634
harshpreet931 <harshpreet0402@gmail.com>
2024-08-28 21:16:44 +0530

    Fibonacci Even Sum

---
+++ Sir_Questions/ritesh_sir_questions/fibonacci_even_sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class fibonacci_even_sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0, b = 1, c = 1;
+        int sum = 0;
+
+        while(c <= n) {
+            if(c % 2 == 0) {
+                System.out.println(c);
+                sum += c;
+            }
+            a = b;
+            b = c;
+            c = a + b;
+        }
+
+        System.out.println(sum);
+    }
+}
