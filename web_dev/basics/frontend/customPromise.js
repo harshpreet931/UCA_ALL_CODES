9eb3893b80a4e93fac208a49cd6d13d8bb8070c3
harshpreet931 <harshpreet0402@gmail.com>
2024-08-30 20:29:32 +0530

    customPromise

---
+++ web_dev/basics/frontend/customPromise.js
@@ -0,0 +1,59 @@
+function promise(e) {
+    let onResolve, onReject;
+    let isFinished = false;
+    let isRejected = false;
+    let value;
+
+    function resolve(val) {
+        isFinished = true;
+        value = val;
+        if(onResolve) onResolve(val);
+    }
+
+    function reject(val) {
+        isFinished = true;
+        isRejected = true;
+        value = val;
+        if(onReject) onReject(val);
+    }
+
+    e(resolve, reject);
+
+    this.then = function(callback) {
+        if(isFinished && !isRejected) {
+            callback(value);
+        }
+        else {
+            onResolve = callback;
+        }
+        return this;
+    }
+
+    this.catch = function(callback) {
+        if(isRejected) {
+            callback(value);
+        }
+        else {
+            onReject = callback;
+        }
+        return this;
+    }
+}
+
+let p = new promise((resolve, reject) => {
+    setTimeout(() => {
+        let success = true;
+        if(success) {
+            resolve('Success');
+        }
+        else {
+            reject('Failure');
+        }
+    }, 1000);
+});
+
+p.then((val) => {
+    console.log(val);
+}).catch((val) => {
+    console.log(val);
+});
