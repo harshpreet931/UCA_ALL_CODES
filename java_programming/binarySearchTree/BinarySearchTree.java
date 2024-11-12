b38bc5955a9bfefa65e63733a251fc926dfa6364
harshpreet931 <harshpreet0402@gmail.com>
2024-11-12 20:46:26 +0530

    Add Binary Search Tree implementation and enhance Lambda examples

---
+++ java_programming/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,237 @@
+package java_programming.binarySearchTree;
+
+class A implements Comparable<A> {
+    int x;
+
+    A() {
+        x = 0;
+    }
+
+    public int compareTo(A o) {
+        if (this.x < o.x)
+            return -1;
+        else if (this.x > o.x)
+            return 1;
+        else
+            return 0;
+    }
+}
+
+public class BinarySearchTree<Key extends Comparable<Key>, Value> 
+{
+    class Node 
+    {
+        Key key;
+        Value value;
+        Node left, right;
+        int count = 0;
+
+        Node(Key key, Value value, int count)
+        {
+            this.key = key;
+            this.value = value;
+            left = right = null;
+            this.count = count;
+        }
+    }
+
+    private Node root;
+    BinarySearchTree()
+    {
+        this.root = null;
+    }
+
+    public Value get(Key key)
+    {
+        return get(root, key);
+    }
+
+    private Value get(Node root, Key key)
+    {
+        if (root == null)
+            return null;
+        if (root.key.equals(key))
+            return root.value;
+        if (root.key.compareTo(key) > 0)
+            return get(root.left, key);
+        else
+            return get(root.right, key);
+    }
+
+    public void put(Key key, Value value)
+    {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node root, Key key, Value value)
+    {
+        if (root == null)
+            return new Node(key, value, 1);
+
+        int cmp = key.compareTo(root.key);
+        if (cmp < 0)
+            root.left = put(root.left, key, value);
+        else if (cmp > 0)
+            root.right = put(root.right, key, value);
+        else
+            root.value = value;
+        
+        root.count = 1 + size(root.left) + size(root.right);
+        return root;
+    }
+
+    public Key floor(Key key)
+    {
+        Node temp = floor(root, key);
+        if(temp == null) return null;
+        return temp.key;
+    }
+
+    private Node floor(Node root, Key key)
+    {
+        if(root == null) return null;
+        
+        if(root.key.equals(key)) return root;
+
+        if(root.key.compareTo(key) > 0)
+            return floor(root.left, key);
+        
+        Node temp = floor(root.right, key);
+        if(temp != null) return temp;
+        else return root;
+    }
+
+    public int size()
+    {
+        return size(root);
+    }
+
+    private int size(Node root)
+    {
+        if(root == null) return 0;
+        return root.count;
+    }
+
+    public int rank(Key key)
+    {
+        return rank(root, key);
+    }
+
+    private int rank(Node root, Key key)
+    {
+        if(root == null) return 0;
+
+        if(root.key.equals(key)) return size(root.left);
+
+        if(root.key.compareTo(key) > 0)
+            return rank(root.left, key);
+        else
+            return size(root) + rank(root.right, key);
+    }
+
+    public Key ceil(Key key)
+    {
+        Node temp = ceil(root, key);
+        if(temp == null) return null;
+        return temp.key;
+    }
+
+    private Node ceil(Node root, Key key)
+    {
+        if(root == null) return null;
+
+        if(root.key.equals(key)) return root;
+
+        if(root.key.compareTo(key) < 0)
+            return ceil(root.right, key);
+        else
+        {
+            Node temp = ceil(root.left, key);
+            if(temp != null) return temp;
+            else return root;
+        }
+    }
+
+    public void inorder_traversal()
+    {
+        inorder_traversal(root);
+    }
+
+    private void inorder_traversal(Node root)
+    {
+        if(root == null) return;
+        inorder_traversal(root.left);
+        System.out.println(root.key + " " + root.value);
+        inorder_traversal(root.right);
+    }
+
+    public void postorder_traversal()
+    {
+        postorder_traversal(root);
+    }
+
+    private void postorder_traversal(Node root)
+    {
+        if(root == null) return;
+        postorder_traversal(root.left);
+        postorder_traversal(root.right);
+        System.out.println(root.key + " " + root.value);
+    }
+
+    public void preorder_traversal()
+    {
+        preorder_traversal(root);
+    }
+
+    private void preorder_traversal(Node root)
+    {
+        if(root == null) return;
+        System.out.println(root.key + " " + root.value);
+        preorder_traversal(root.left);
+        preorder_traversal(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree<A, Integer> bst = new BinarySearchTree<A, Integer>();
+        A a = new A();
+        a.x = 10;
+        bst.put(a, 10);
+
+        A b = new A();
+        b.x = 20;
+        bst.put(b, 20);
+
+        A c = new A();
+        c.x = 5;
+        bst.put(c, 5);
+
+        System.out.println(bst.get(a));
+        System.out.println(bst.get(b));
+        System.out.println(bst.get(c));
+
+        BinarySearchTree<Integer, Integer> bst1 = new BinarySearchTree<Integer, Integer>();
+        bst1.put(10, 20);
+        bst1.put(20, 40);
+        bst1.put(5, 10);
+
+        System.out.println(bst1.get(10));
+        System.out.println(bst1.get(20));
+        System.out.println(bst1.get(5));
+
+        System.out.println(bst1.floor(7));
+
+        System.out.println(bst1.size());
+
+        System.out.println(bst1.rank(20));
+
+        System.out.println(bst1.ceil(7));
+
+        bst1.inorder_traversal();
+        System.out.println();
+        bst1.postorder_traversal();
+        System.out.println();
+        bst1.preorder_traversal();
+
+
+    }
+}
--- java_programming/lambdas/Lambda.java
+++ java_programming/lambdas/Lambda.java
@@ -1,6 +1,7 @@
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.function.Function;
 import java.util.*;
 import java.io.*;
@@ -42,5 +43,45 @@ public class Lambda {
         };
 
         System.out.println(getStringLen.apply("Hello World")); // 11
+
+        // Examples
+
+        // Function -> take in integer, return sqrt
+        Function<Integer, Double> sqrt = (i) -> Math.sqrt(i);
+        System.out.println(sqrt.apply(4));
+
+        // Function -> taking string and return uppercase
+        Function<String, String> upperCase = (s) -> s.toUpperCase();
+        System.out.println(upperCase.apply("hello world"));
+
+        // Function -> taking string and returning a string with removed white spaces from front and end
+        Function<String, String> trim = (s) -> s.trim();
+        System.out.println(trim.apply("  hello world  "));
+
+        // Function -> Supplier, Consumer. Supplier should gen a RandomUUID and Consumer should log it 10 times in a interval of 2 seconds
+        Function<Supplier<String>, Consumer<String>> function = (supplier) -> {
+            Consumer<String> cons = (s) -> {
+                for (int i = 0; i < 10; i++) {
+                    String id = supplier.get();
+                    System.out.println(id);
+                }
+            };
+            return cons;
+        };
+
+        function.apply(
+            () -> UUID.randomUUID().toString()
+        ).accept("Hello");
+
+
+        // Streams
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        List<Integer> oddList = list.stream().filter((i) -> i % 2 != 0)
+                                             .map((i) -> i * i)
+                                             .collect(Collectors.toList());
+        System.out.println(oddList);
+
     }
 }
